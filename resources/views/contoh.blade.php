74391501640d6508c14223f86e243fadccf7f9aa
fadli1003 <huzhain@gmail.com>
2026-02-03 08:35:33 +0700

    SIMRS initial commit

---
+++ resources/views/contoh.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> @yield('page_title', 'Dashboard') | {{config('app.name', 'RS UNAND' )}}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    
+</body>
+</html>
